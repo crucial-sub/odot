0038811f5416a444fac524ee0a7f244ca44f811b
crucial-sub <jssub940@gmail.com>
2024-02-14 04:49:29 +0900

    header 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,48 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Image, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {ImageAssets} from './assets/images/ImageAssets';
 
 function App(): React.JSX.Element {
+  const [userName, setUserName] = useState('Crucial-Sub');
+
   return (
-    <View>
-      <Text>Hello World!!</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.wrapper}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Hello {userName}👋</Text>
+          <Image
+            source={ImageAssets.profileImage}
+            style={styles.profileImage}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  wrapper: {
+    width: '90%',
+    height: '100%',
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  profileImage: {
+    width: 35,
+    height: 35,
+  },
+});
 
 export default App;
---
+++ src/assets/images/ImageAssets.ts
@@ -0,0 +1,3 @@
+export const ImageAssets = {
+  profileImage: require('./crusub.png'),
+};
