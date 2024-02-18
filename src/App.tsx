66481ef94496d049871bf3d0733f49602b050cec
crucial-sub <jssub940@gmail.com>
2024-02-18 14:20:50 +0900

    recoil 오류 해결

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import {StyleSheet} from 'react-native';
 import {RecoilRoot} from 'recoil';
 import MainScreen from './screens/MainScreen';
+import {RecoilEnv} from 'recoil';
+
+RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
 
 function App(): React.JSX.Element {
   return (
