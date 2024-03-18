f17814dfad7eb7e86490e62ac5289f774fe122e0
crucial-sub <jssub940@gmail.com>
2024-03-18 19:24:37 +0900

    ToastMessage UI

--- src/components/ToastMessage/ToastMessage.tsx
+++ src/components/ToastMessage/ToastMessage.tsx
@@ -1,13 +1,33 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 import Animated from 'react-native-reanimated';
+import {useRecoilValue} from 'recoil';
+import {toastState, toastVisibleState} from '../../recoil';
+import useToastMessage from '../hooks/useToastMessage';
+import ToastIcon from './ToastIcon';
 
-const TOAST_SIZE = 300;
+const TOAST_SIZE = 340;
 
 const ToastMessage = () => {
+  const {getToastStyle} = useToastMessage();
+  const toastVisible = useRecoilValue(toastVisibleState);
+  const {type, message} = useRecoilValue(toastState);
+  const [isVisible, setIsVisible] = React.useState(false);
+  const title = type.charAt(0).toUpperCase() + type.slice(1);
+
+  React.useEffect(() => {
+    setIsVisible(toastVisible);
+  }, [toastVisible]);
+
+  if (!isVisible) return null;
+
   return (
-    <Animated.View style={[styles.toastWrapper]}>
-      <Text>ToastMessage</Text>
+    <Animated.View style={[styles.toastWrapper, getToastStyle(type)]}>
+      <ToastIcon type={type} />
+      <View style={styles.textBox}>
+        <Text style={styles.toastTitle}>{title}</Text>
+        <Text style={styles.messageText}>{message}</Text>
+      </View>
     </Animated.View>
   );
 };
@@ -18,11 +38,24 @@ const styles = StyleSheet.create({
   toastWrapper: {
     position: 'absolute',
     width: TOAST_SIZE,
-    height: 80,
-    backgroundColor: 'red',
+    height: 70,
     top: 60,
     left: '50%',
     transform: [{translateX: -TOAST_SIZE / 2}],
     borderRadius: 12,
+    borderWidth: 2,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 12,
+    gap: 15,
+  },
+  textBox: {gap: 3},
+  toastTitle: {
+    fontWeight: '700',
+    fontSize: 16,
+    color: '#333439',
+  },
+  messageText: {
+    color: '#626463',
   },
 });
